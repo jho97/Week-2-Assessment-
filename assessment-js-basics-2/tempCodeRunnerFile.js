84dbc102e11d66ebd64947cc268a8d178e729d97
Jonathan Ho <98234461+jho97@users.noreply.github.com>
2022-04-01 13:50:49 -0500

    Add files via upload

---
+++ assessment-js-basics-2/tempCodeRunnerFile.js
@@ -0,0 +1,30 @@
+
+const pizza = {
+    name: 'cheese',
+    price: 2.00,
+    category: 'entree',
+    popularity: 1,
+    rating: 1,
+    tags: ['cheese, bread']
+
+}
+
+//////////////////PROBLEM 2////////////////////
+/* 
+    Let's print a few values from our pizza object.
+
+    First, log the popularity of pizza. 
+    Use dot notation to access the value.
+*/
+
+console.log(pizza.popularity)
+
+
+/*
+    Second, log the second tag in your pizza's
+    tags array.
+    Use a combination of dots and brackets to
+    get the value.
+*/
+
+console.log(pizza.price)
---
+++ figma.md
@@ -0,0 +1,2 @@
+
+https://www.figma.com/file/cODDkuVkVNdjVG89qpWLyw/%22PANDA%22-WEB-DESIGN?node-id=0%3A1
